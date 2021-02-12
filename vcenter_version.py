5f8a97113e6e4bef4117a4daf307509637fe01f7
Luthfi Anandra <luthfi.anandra@gmail.com>
2021-02-13 00:08:46 +0700

    Create vcenter_version.py

---
+++ vcenter_version.py
@@ -0,0 +1,20 @@
+# Sample script for generating vcenter version
+
+import requests
+import urllib3
+from com.vmware.appliance.system_client import Version
+from vmware.vapi.vsphere.client import create_vsphere_client
+session = requests.session()
+
+# Disable cert verification
+session.verify = False
+
+# Disable the secure connection warning
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# Connect to a vCenter Server using username and password
+vsphere_client = create_vsphere_client(server="ip_address", username="your_username", password="your_password", session=session)
+
+# Get vCenter version
+vcenter_version = vsphere_client.appliance.system.Version.get()
+print(vcenter_version)
